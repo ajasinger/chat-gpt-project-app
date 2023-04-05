033993ea496e8d9289373d6899e01ecad1c38615
Aja Singer <ajasinger@gmail.com>
2023-04-05 16:54:43 -0400

    define API call and response

--- pages/api/inspire.ts
+++ pages/api/inspire.ts
@@ -1,13 +1,42 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { Configuration, OpenAIApi } from 'openai'
 
-type Data = {
-  name: string
-}
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+});
 
-export default function handler(
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  //send prompt to server
+  const prompt = req.query.prompt;
+
+  if(!prompt) {
+    return res.status(400).json({error: "Prompt missing"})
+  }
+
+  if(prompt.length > 100) {
+    return res.status(400).json({error: "Prompt over 100 characters"})
+  }
+
+  //instructions for how chatGPT should answer
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `Create an inspiring motivational quote based on the following topic. \n
+    Topic:${prompt}\n
+    Motivational quote:`,
+    max_tokens: 500, 
+    temperature: 1,
+    presence_penalty: 0,
+    frequency_penalty: 0
+  });
+
+  //send response to front-end 
+  const quote = completion.data.choices[0].text;
+
+  res.status(200).json({ quote })
 }
