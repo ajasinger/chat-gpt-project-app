f50bb3cbc16eb79358a93571ae7a5c4c4a607c1b
Aja Singer <ajasinger@gmail.com>
2023-04-05 12:07:56 -0400

    add image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import mainImage from '@/assets/images/fab-lentz-mRMQwK513hY-unsplash.jpg'
 
 export default function Home() {
   return (
@@ -16,6 +16,12 @@ export default function Home() {
         <h1>Motivate Me AI</h1>
         <h2>powered by GPT-3</h2>
         <div>Enter a topic and the AI will generate a motivational quote.</div>
+        <div className={styles.mainImageContainer}>
+          <Image 
+          src={mainImage}
+          fill
+          />
+        </div>
       </main>
     </>
   )
